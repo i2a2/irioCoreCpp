a60150520bc05c735b329f2c47c84b11cf28412a
Víctor Costa <victor.costa.perez@upm.es>
2024-02-19 21:38:05 +0100

    Modify test to assert the number of found resources are correct

--- src/test/c++/irio_v2_cpp/TP_FlexRIO.cpp
+++ src/test/c++/irio_v2_cpp/TP_FlexRIO.cpp
@@ -9,12 +9,14 @@ TEST(FlexRIO, ResourcesCPUDAQ){
 
    IrioV2 irio(bitfile, "", "0x0177A2AD", "4.0", "TEST", true); //TODO: Fix this with actual values
 
-   auto numAI = irio.analog()->getNumAI();
-   ASSERT_EQ(numAI, 2);
-
-   auto b = irio.analog()->getAI(0);
-
-   irio.digital();
-
+   EXPECT_EQ(irio.analog()->getNumAI(), 2) << "Unexpected number of analog inputs";
+   EXPECT_EQ(irio.analog()->getNumAO(), 2) << "Unexpected number of analog outputs";
+   EXPECT_EQ(irio.digital()->getNumDI(), 2) << "Unexpected number of digital inputs";
+   EXPECT_EQ(irio.digital()->getNumDO(), 2) << "Unexpected number of digital outputs";
+   EXPECT_EQ(irio.auxAnalog()->getNumAuxAI(), 2) << "Unexpected number of aux analog inputs";
+   EXPECT_EQ(irio.auxAnalog()->getNumAuxAO(), 2) << "Unexpected number of aux analog outputs";
+   EXPECT_EQ(irio.auxDigital()->getNumAuxDI(), 2) << "Unexpected number of aux digital inputs";
+   EXPECT_EQ(irio.auxDigital()->getNumAuxDO(), 2) << "Unexpected number of aux digital outputs";
+   EXPECT_EQ(irio.signalGeneration()->getSGNo(), 2) << "Unexpected number of signal generators";
 }
 
