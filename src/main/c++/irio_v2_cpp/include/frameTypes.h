aee60da3bbd4f50b9c2f5379a2aaafc584b5bdda
Víctor Costa <victor.costa.perez@upm.es>
2024-02-29 14:54:12 +0100

    Add missing namespace to FrameTypes

--- src/main/c++/irio_v2_cpp/include/frameTypes.h
+++ src/main/c++/irio_v2_cpp/include/frameTypes.h
@@ -1,13 +1,15 @@
 #pragma once
 
-	/**
-	 * Data format of the data read
-	 *
-	 * @ingroup DMATerminals
-	 */
-	enum class FrameType{
-		FormatA = 0,/**< Format used for DAQ samples */
-		FormatB = 1 /**< Not yet defined */
-	};
+namespace iriov2{
 
+/**
+ * Data format of the data read
+ *
+ * @ingroup DMATerminals
+ */
+enum class FrameType {
+	FormatA = 0,/**< Format used for DAQ samples */
+	FormatB = 1 /**< Not yet defined */
+};
 
+}
