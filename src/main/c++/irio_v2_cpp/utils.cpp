4dd76c306213c74c76a5e56e2a6efefbde8456d1
Víctor Costa <victor.costa.perez@upm.es>
2024-03-22 00:15:22 +0100

    Remove unused util functions

--- src/main/c++/irio_v2_cpp/utils.cpp
+++ src/main/c++/irio_v2_cpp/utils.cpp
@@ -16,36 +16,6 @@ void throwIfNotSuccessNiFpga(const NiFpga_Status &status,
 	}
 }
 
-template<typename T>
-void findAndInsertEnumResources(
-		const std::unordered_map<std::string, T> &mapSearch,
-		const std::string &resourceName, const size_t maxElem,
-		std::unordered_map<std::uint32_t, const std::uint32_t> *mapInsert) {
-	static_assert(std::is_base_of<bfp::Resource, T>::value,
-			"T must inherit from bfp::Resource");
-
-	for (size_t i = 0; i < maxElem; ++i) {
-		const auto it = mapSearch.find(resourceName + std::to_string(i));
-		if (it != mapSearch.end()) {
-			mapInsert->insert({ i, it->second.getAddress() });
-		}
-	}
-}
-
-void findAndInsertEnumRegisters(const bfp::BFP &parsedBitfile,
-		const std::string &terminalName, const size_t maxElem,
-		std::unordered_map<std::uint32_t, const std::uint32_t> *mapInsert) {
-	findAndInsertEnumResources(parsedBitfile.getRegisters(), terminalName,
-			maxElem, mapInsert);
-}
-
-void findAndInsertEnumDMAs(const bfp::BFP &parsedBitfile,
-		const std::string &dmaName, const size_t maxElem,
-		std::unordered_map<std::uint32_t, const std::uint32_t> *mapInsert) {
-	findAndInsertEnumResources(parsedBitfile.getDMAs(), dmaName, maxElem,
-			mapInsert);
-}
-
 std::uint32_t getAddressEnumResource(
 		const std::unordered_map<std::uint32_t, const std::uint32_t> &mapResource,
 		const std::uint32_t n, const std::string &resourceName) {
@@ -57,32 +27,6 @@ std::uint32_t getAddressEnumResource(
 	return it->second;
 }
 
-template<typename T>
-void findArrayRegReadToVector(const bfp::BFP &parsedBitfile,
-		const NiFpga_Session &session, const std::string &nameReg,
-		std::vector<T> *vec,
-		std::function<NiFpga_Status(NiFpga_Session,
-				std::uint32_t, T*, size_t)> readFunc) {
-	const auto reg = parsedBitfile.getRegister(nameReg);
-	vec->resize(reg.getNumElem());
-	const auto status = readFunc(session, reg.getAddress(), vec->data(), vec->size());
-	utils::throwIfNotSuccessNiFpga(status, "Error reading " + nameReg);
-}
-
-
-template void findArrayRegReadToVector<std::uint8_t>(
-		const bfp::BFP &parsedBitfile, const NiFpga_Session &session,
-		const std::string &nameReg, std::vector<std::uint8_t> *vec,
-		std::function<
-				NiFpga_Status(NiFpga_Session, std::uint32_t, std::uint8_t*,
-						size_t)> readFunc);
-
-template void findArrayRegReadToVector<std::uint16_t>(
-		const bfp::BFP &parsedBitfile, const NiFpga_Session &session,
-		const std::string &nameReg, std::vector<std::uint16_t> *vec,
-		std::function<
-				NiFpga_Status(NiFpga_Session, std::uint32_t, std::uint16_t*,
-						size_t)> readFunc);
 
 }  // namespace utils
 }  // namespace iriov2
