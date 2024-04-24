71d8c099a2a85823b07a3995d85d793549769695
Víctor Costa <victor.costa.perez@upm.es>
2024-04-24 09:06:17 +0200

    Move doc folder from c++ folder to the main folder

--- workflowStages/documentation.mk
+++ workflowStages/documentation.mk
@@ -2,6 +2,6 @@ all: doc
 
 doc:
 	@printf "$(BOLD)Generating documentation...$(NC)\n"
-	@mkdir -p $(COPY_DIR)/doc/irioCore
-	@sed -i 's/{VERSION_DOXYGEN}/$(VERSION)/g' $(COPY_DIR)/main/c++/doc/Doxyfile
-	$(MAKE) -C $(COPY_DIR)/main/c++/doc all	
+	@mkdir -p $(COPY_DIR)/doc/irioCoreCpp
+	@sed -i 's/{VERSION_DOXYGEN}/$(VERSION)/g' $(COPY_DIR)/main/doc/Doxyfile
+	$(MAKE) -C $(COPY_DIR)/main/doc all	
