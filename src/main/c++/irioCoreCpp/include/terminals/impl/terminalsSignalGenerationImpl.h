3021ae5bb3e4f32f960a56400b47fc8525192da0
Víctor Costa <victor.costa.perez@upm.es>
2024-04-25 13:37:55 +0200

    Fix line too long

--- src/main/c++/irioCoreCpp/include/terminals/impl/terminalsSignalGenerationImpl.h
+++ src/main/c++/irioCoreCpp/include/terminals/impl/terminalsSignalGenerationImpl.h
@@ -41,7 +41,8 @@ class TerminalsSignalGenerationImpl: public TerminalsBaseImpl {
 
 	void setSGAmpImpl(const std::uint32_t n, const std::uint32_t value) const;
 
-	void setSGFreqDecimationImpl(const std::uint32_t n, const std::uint32_t value) const;
+	void setSGFreqDecimationImpl(const std::uint32_t n,
+								 const std::uint32_t value) const;
 
 	void setSGPhaseImpl(const std::uint32_t n, const std::uint32_t value) const;
 
