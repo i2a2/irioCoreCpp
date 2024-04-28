7df57317c51f56faa4ff2ab741bc90eb77b22218
Victor Costa <victor.costa.perez@upm.es>
2024-04-29 00:03:58 +0200

    Refactor how packaging is done to use a common Makefile to build the rpms

--- workflowStages/packaging/package_doc.mk
+++ workflowStages/packaging/package_doc.mk
@@ -1,53 +1,20 @@
 BOLD=\e[1m
 NC=\e[0m
 
-PACKAGE_NAME := irioCore_doc
+PACKAGE_NAME := irioCoreCpp_doc
 
-HTML_DOC_FOLDER := $(TOP_DIR)/$(COPY_DIR)/doc/irioCore/html/*
+HTML_DOC_FOLDER := doc/irioCoreCpp/html
 
-DOC_INSTALL_DIR := $(BASE_DOC_INSTALL_DIR)
 
-FILES_SPEC = $(DOC_INSTALL_DIR)/irioCore/*
-FILES_SPEC_FINAL := $(shell echo "$(FILES_SPEC)" | sed 's/\//\\\//g' | sed 's/ /\{NEWLINE\}/g')
+FILES=$(HTML_DOC_FOLDER):$(BASE_DOC_INSTALL_DIR)/irioCoreCpp/
 
-RPM_BUILD_DIR := $(TOP_DIR)/$(COPY_DIR)/rpmbuild/$(PACKAGE_NAME)
+SPEC_FILE=$(realpath ./rpmspecs/$(PACKAGE_NAME).spec)
 
-RPM_OUTPUT_DIR := $(RPM_BUILD_DIR)/RPMS
-RPM_SOURCE_DIR := $(RPM_BUILD_DIR)/SOURCES
-RPM_BUILD_ROOT := $(RPM_BUILD_DIR)/BUILDROOT
-RPM_SPECS_DIR := $(RPM_BUILD_DIR)/SPECS
-
-RPM_SPEC_FILE := $(PACKAGE_NAME).spec
-RPM_SPEC_FILE_ORIG := rpmspecs/$(RPM_SPEC_FILE)
-RPM_SPEC_FILE_DEST := $(RPM_SPECS_DIR)/$(RPM_SPEC_FILE)
-
-# Define the RPM build command
-RPM_BUILD_CMD := rpmbuild
-
-# Define the RPM build options
-RPM_BUILD_OPTIONS := -bb
+.PHONY: package
 
 all: package
 
-clean:
-	@rm -rf $(BUILD_DIR) $(RPM_BUILD_DIR)
-
-package: clean gen_rpmbuild
-	cp -r $(HTML_DOC_FOLDER) $(RPM_BUILD_ROOT)/$(DOC_INSTALL_DIR)/irioCore
-	cp $(RPM_SPEC_FILE_ORIG) $(RPM_SPEC_FILE_DEST)
-	sed -i 's/{VERSION}/$(VERSION)/g' $(RPM_SPEC_FILE_DEST)
-	sed -i 's/{FILES_TO_INCLUDE}/$(FILES_SPEC_FINAL)/g' $(RPM_SPEC_FILE_DEST)
-	sed -i 's/{NEWLINE}/\n/g' $(RPM_SPEC_FILE_DEST)
-	$(RPM_BUILD_CMD) --define "_rpmdir $(PWD)/$(TOP_DIR)/$(COPY_DIR)/packages" --buildroot $(PWD)/$(RPM_BUILD_ROOT) $(RPM_BUILD_OPTIONS) $(RPM_SPEC_FILE_DEST)
-
-gen_rpmbuild:
-	printf "$(BOLD)Generating $(PACKAGE_NAME) package...$(NC)\n"
-	@mkdir -p $(RPM_BUILD_DIR)/BUILD
-	@mkdir -p $(RPM_BUILD_DIR)/BUILDROOT
-	@mkdir -p $(RPM_BUILD_DIR)/RPMS
-	@mkdir -p $(RPM_BUILD_DIR)/SOURCES
-	@mkdir -p $(RPM_BUILD_DIR)/SPECS
-	@mkdir -p $(RPM_BUILD_DIR)/SRPMS
-	@mkdir -p $(RPM_BUILD_ROOT)/$(DOC_INSTALL_DIR)/irioCore
-
-.PHONY: clean build
+package:
+	$(MAKE) -f $(PACKAGE_MK) FILES=$(FILES) SPEC_FILE=$(SPEC_FILE)\
+		PACKAGE_NAME=$(PACKAGE_NAME) PACKAGE_VERSION=$(VERSION)\
+		BASE_DIR=$(TOP_DIR)/$(COPY_DIR) OUTPUT_DIR=$(TOP_DIR)/$(COPY_DIR)/packages
--- workflowStages/packaging/package_irioCore.mk
+++ workflowStages/packaging/package_irioCore.mk
@@ -1,58 +1,21 @@
-BOLD=\e[1m
-NC=\e[0m
-
 PACKAGE_NAME := irioCore
 
-LIBRARIES := $(TOP_DIR)/$(COPY_DIR)/lib/lib$(PACKAGE_NAME).so
+LIBRARIES := lib/lib$(PACKAGE_NAME).so
 INCLUDES :=
 
 LIB_INSTALL_DIR := $(BASE_LIB_INSTALL_DIR)
 INC_INSTALL_DIR := $(BASE_INC_INSTALL_DIR)/$(PACKAGE_NAME)
 
-FILES_SPEC = $(foreach file,$(notdir $(LIBRARIES)),"$(LIB_INSTALL_DIR)/$(file)"\n)
-FILES_SPEC += $(foreach file,$(notdir $(INCLUDES)),"$(INC_INSTALL_DIR)/$(file)"\n)
-FILES_SPEC_FINAL := $(shell echo "$(FILES_SPEC)" | sed 's/\//\\\//g' | sed 's/ /\{NEWLINE\}/g')
+FILES=$(foreach file,$(LIBRARIES),$(file):$(LIB_INSTALL_DIR)/$(notdir $(file)))
+FILES+=$(foreach file,$(INCLUDES),$(file):$(INC_INSTALL_DIR)/$(notdir $(file)))
 
-RPM_BUILD_DIR := $(TOP_DIR)/$(COPY_DIR)/rpmbuild/$(PACKAGE_NAME)
+SPEC_FILE=$(realpath ./rpmspecs/$(PACKAGE_NAME).spec)
 
-RPM_OUTPUT_DIR := $(RPM_BUILD_DIR)/RPMS
-RPM_SOURCE_DIR := $(RPM_BUILD_DIR)/SOURCES
-RPM_BUILD_ROOT := $(RPM_BUILD_DIR)/BUILDROOT
-RPM_SPECS_DIR := $(RPM_BUILD_DIR)/SPECS
-
-RPM_SPEC_FILE := $(PACKAGE_NAME).spec
-RPM_SPEC_FILE_ORIG := rpmspecs/$(RPM_SPEC_FILE)
-RPM_SPEC_FILE_DEST := $(RPM_SPECS_DIR)/$(RPM_SPEC_FILE)
-
-# Define the RPM build command
-RPM_BUILD_CMD := rpmbuild
-
-# Define the RPM build options
-RPM_BUILD_OPTIONS := -bb
+.PHONY: package
 
 all: package
 
-clean:
-	@rm -rf $(BUILD_DIR) $(RPM_BUILD_DIR)
-
-package: clean gen_rpmbuild
-	@cp $(LIBRARIES) $(RPM_BUILD_ROOT)/$(LIB_INSTALL_DIR)
-	@cp $(RPM_SPEC_FILE_ORIG) $(RPM_SPEC_FILE_DEST)
-	@sed -i 's/{VERSION}/$(VERSION)/g' $(RPM_SPEC_FILE_DEST)
-	sed -i 's/{FILES_TO_INCLUDE}/$(FILES_SPEC_FINAL)/g' $(RPM_SPEC_FILE_DEST)
-	sed -i 's/{NEWLINE}/\n/g' $(RPM_SPEC_FILE_DEST)
-	$(RPM_BUILD_CMD) --define "_rpmdir $(PWD)/$(TOP_DIR)/$(COPY_DIR)/packages" --buildroot $(PWD)/$(RPM_BUILD_ROOT) $(RPM_BUILD_OPTIONS) $(RPM_SPEC_FILE_DEST)
-
-gen_rpmbuild:
-	printf "$(BOLD)Generating $(PACKAGE_NAME) package...$(NC)\n"
-	@mkdir -p $(RPM_BUILD_DIR)/BUILD
-	@mkdir -p $(RPM_BUILD_DIR)/BUILDROOT
-	@mkdir -p $(RPM_BUILD_DIR)/RPMS
-	@mkdir -p $(RPM_BUILD_DIR)/SOURCES
-	@mkdir -p $(RPM_BUILD_DIR)/SPECS
-	@mkdir -p $(RPM_BUILD_DIR)/SRPMS
-	@mkdir -p $(RPM_BUILD_ROOT)/$(LIB_INSTALL_DIR)
-	@mkdir -p $(RPM_BUILD_ROOT)/$(INC_INSTALL_DIR)
-
-
-.PHONY: clean build
+package:
+	$(MAKE) -f $(PACKAGE_MK) FILES=$(FILES) SPEC_FILE=$(SPEC_FILE)\
+		PACKAGE_NAME=$(PACKAGE_NAME) PACKAGE_VERSION=$(VERSION)\
+		BASE_DIR=$(TOP_DIR)/$(COPY_DIR) OUTPUT_DIR=$(TOP_DIR)/$(COPY_DIR)/packages
