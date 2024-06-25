f23d5c29b370d3233125d66cf1afbd7adbb97a5f
Victor Costa <victor.costa.perez@upm.es>
2024-06-25 17:19:47 +0200

    Update RSeries tests

--- src/test/c++/irioCoreCpp/TP_Rseries_CPUDAQ.cpp
+++ src/test/c++/irioCoreCpp/TP_Rseries_CPUDAQ.cpp
@@ -119,7 +119,278 @@ TEST_F(RSeriesCPUDAQ, GetSetSGPhase) {
 	EXPECT_EQ(phaseRead, phase);
 }
 
-/// Error FlexRIO DAQ Tests
+
+TEST_F(RSeriesCPUDAQ, GetSetEnableAO) {
+	int32_t channel = 0;
+
+	const std::string bitfilePath = getBitfilePath();
+	Irio irio(bitfilePath, serialNumber, "V1.2");
+	auto analog = irio.getTerminalsAnalog();
+	irio.startFPGA();
+
+	EXPECT_NO_THROW(analog.setAOEnable(channel, true));
+	bool aoEnable;
+	EXPECT_NO_THROW(aoEnable = analog.getAOEnable(channel));
+	EXPECT_EQ(aoEnable, true);
+
+	EXPECT_NO_THROW(analog.setAOEnable(channel, false));
+	EXPECT_NO_THROW(aoEnable = analog.getAOEnable(channel));
+	EXPECT_EQ(aoEnable, false);
+}
+
+
+TEST_F(RSeriesCPUDAQ, GetSetAO) {
+	int32_t channel = 0;
+
+	// Initialize random engine
+	int min = 0, max = 100;
+	std::mt19937 gen(
+		std::chrono::system_clock::now().time_since_epoch().count());
+	std::uniform_int_distribution<int64_t> dist(min + 1, max - 1);
+
+	const std::string bitfilePath = getBitfilePath();
+	Irio irio(bitfilePath, serialNumber, "V1.2");
+	auto analog = irio.getTerminalsAnalog();
+	irio.startFPGA();
+
+	analog.setAOEnable(channel, true);
+
+	try {
+		std::vector<int64_t> values = {min, dist(gen), dist(gen), dist(gen),
+									   max};
+		for (const auto& v : values) {
+			// Write AO
+			analog.setAO(channel, v);
+
+			// Read AO
+			auto readV = analog.getAO(channel);
+			EXPECT_EQ(readV, v) << "Value read from AO is incorrect";
+
+			// Check the value in auxAI9
+			readV = irio.getTerminalsAuxAnalog().getAuxAI(9);
+			EXPECT_EQ(readV, v) << "Value read from auxAI9 is incorrect";
+		}
+	} catch (errors::IrioError& e) {
+		FAIL() << e.what();
+	}
+}
+
+TEST_F(RSeriesCPUDAQ, cleanAllDMAs) {
+	const std::string bitfilePath = getBitfilePath();
+	Irio irio(bitfilePath, serialNumber, "V1.2");
+	irio.startFPGA();
+
+	auto daq = irio.getTerminalsDAQ();
+	EXPECT_NO_THROW(daq.cleanAllDMAs());
+}
+
+TEST_F(RSeriesCPUDAQ, readDataNonBlocking){
+	const std::string bitfilePath = getBitfilePath();
+	const size_t blocksToRead = 1;
+	const std::uint32_t samplingRate = 500000;
+	const std::uint32_t channelSG = 0;
+	const std::uint32_t DMANum = 0;
+	const std::uint32_t channelDMA = 2;
+	const std::uint32_t analogNum = 0;
+	const std::uint32_t valDCSG = 2048;
+	const std::uint8_t signalType = 0; //DC
+
+	Irio irio(bitfilePath, serialNumber, "V1.2");
+	const auto commonTerm = irio.getTerminalsCommon();
+	irio.startFPGA();
+	commonTerm.setDebugMode(false);
+
+	auto sg = irio.getTerminalsSignalGeneration();
+	auto analog = irio.getTerminalsAnalog();
+	auto daq = irio.getTerminalsDAQ();
+
+	// Enable AO0 and set to 2048 and configure SGSignalType0
+	sg.setSGSignalType(channelSG, signalType);
+	analog.setAO(analogNum, valDCSG);
+	analog.setAOEnable(analogNum, true);
+
+	// DMA DAQ Parameters
+	const auto nCh = daq.getNCh(DMANum);
+	const auto lengthBlock = daq.getLengthBlock(DMANum);
+
+	// Setup DMAQ
+	const std::uint16_t decimation = commonTerm.getFref()/samplingRate;
+	daq.startDMA(DMANum);
+	daq.setSamplingRateDecimation(DMANum, decimation);
+	daq.enableDMA(DMANum);
+	commonTerm.setDAQStart();
+
+	::usleep(100000);
+
+	std::unique_ptr<std::uint64_t> data(new std::uint64_t[lengthBlock*blocksToRead]);
+	auto sizeRead = daq.readDataNonBlocking(DMANum, lengthBlock*blocksToRead, data.get());
+	EXPECT_EQ(sizeRead, lengthBlock*blocksToRead) << "Incorrect number of elements read";
+
+	daq.disableDMA(DMANum);
+	commonTerm.setDAQStop();
+	sleep(5);
+	daq.cleanDMA(DMANum);
+
+	std::uint16_t *dataAux = reinterpret_cast<std::uint16_t*>(data.get());
+	for (size_t i = 0; i < blocksToRead * lengthBlock; i++) {
+		if (dataAux[(i * nCh) + channelDMA] != valDCSG) {
+			FAIL() << "Incorrect data read";
+		}
+	}
+}
+
+TEST_F(RSeriesCPUDAQ, readDataNonBlockingNoData){
+	const std::string bitfilePath = getBitfilePath();
+	const size_t blocksToRead = 1;
+	const std::uint32_t samplingRate = 500000;
+	const std::uint32_t channelSG = 0;
+	const std::uint32_t DMANum = 0;
+	const std::uint32_t analogNum = 0;
+	const std::uint32_t valDCSG = 2048;
+	const std::uint8_t signalType = 0; //DC
+
+	Irio irio(bitfilePath, serialNumber, "V1.2");
+	const auto commonTerm = irio.getTerminalsCommon();
+	irio.startFPGA();
+	commonTerm.setDebugMode(false);
+
+	auto sg = irio.getTerminalsSignalGeneration();
+	auto analog = irio.getTerminalsAnalog();
+	auto daq = irio.getTerminalsDAQ();
+
+	// Enable AO0 and set to 2048 and configure SGSignalType0
+	sg.setSGSignalType(channelSG, signalType);
+	analog.setAO(analogNum, valDCSG);
+	analog.setAOEnable(analogNum, true);
+
+	// DMA DAQ Parameters
+	const auto lengthBlock = daq.getLengthBlock(DMANum);
+
+	// Setup DMAQ
+	const std::uint16_t decimation = commonTerm.getFref()/samplingRate;
+	daq.startDMA(DMANum);
+	daq.setSamplingRateDecimation(DMANum, decimation);
+	daq.enableDMA(DMANum);
+
+	::usleep(100000);
+
+	std::unique_ptr<std::uint64_t> data(new std::uint64_t[lengthBlock*blocksToRead]);
+	auto sizeRead = daq.readDataNonBlocking(DMANum, lengthBlock*blocksToRead, data.get());
+	EXPECT_EQ(sizeRead, 0) << "Data was read when it was not expected";
+
+	daq.disableDMA(DMANum);
+	commonTerm.setDAQStop();
+	sleep(5);
+	daq.cleanDMA(DMANum);
+}
+
+TEST_F(RSeriesCPUDAQ, readDataBlocking){
+	const std::string bitfilePath = getBitfilePath();
+	const size_t blocksToRead = 1;
+	const std::uint32_t samplingRate = 500000;
+	const std::uint32_t channelSG = 0;
+	const std::uint32_t DMANum = 0;
+	const std::uint32_t channelDMA = 2;
+	const std::uint32_t analogNum = 0;
+	const std::uint32_t valDCSG = 2048;
+	const std::uint8_t signalType = 0; //DC
+
+	Irio irio(bitfilePath, serialNumber, "V1.2");
+	const auto commonTerm = irio.getTerminalsCommon();
+	irio.startFPGA();
+	commonTerm.setDebugMode(false);
+
+	auto sg = irio.getTerminalsSignalGeneration();
+	auto analog = irio.getTerminalsAnalog();
+	auto daq = irio.getTerminalsDAQ();
+
+	// Enable AO0 and set to 2048 and configure SGSignalType0
+	sg.setSGSignalType(channelSG, signalType);
+	analog.setAO(analogNum, valDCSG);
+	analog.setAOEnable(analogNum, true);
+
+	// DMA DAQ Parameters
+	const auto nCh = daq.getNCh(DMANum);
+	const auto lengthBlock = daq.getLengthBlock(DMANum);
+
+	// Setup DMAQ
+	const std::uint16_t decimation = commonTerm.getFref()/samplingRate;
+	std::unique_ptr<std::uint64_t> data(new std::uint64_t[lengthBlock*blocksToRead]);
+	const auto timeout = static_cast<std::uint32_t>(1.5*std::ceil(lengthBlock*blocksToRead*1000/samplingRate));
+
+	daq.startDMA(DMANum);
+	daq.setSamplingRateDecimation(DMANum, decimation);
+	daq.enableDMA(DMANum);
+	commonTerm.setDAQStart();
+
+	try{
+		daq.readDataBlocking(DMANum, lengthBlock*blocksToRead, data.get(), timeout);
+	}catch(errors::DMAReadTimeout&){
+		FAIL() << "Timeout occurred reading DMA when it should have not";
+	}
+
+	daq.disableDMA(DMANum);
+	commonTerm.setDAQStop();
+	sleep(5);
+	daq.cleanDMA(DMANum);
+
+	std::uint16_t *dataAux = reinterpret_cast<std::uint16_t*>(data.get());
+	for (size_t i = 0; i < blocksToRead * lengthBlock; i++) {
+		if (dataAux[(i * nCh) + channelDMA] != valDCSG) {
+			FAIL() << "Incorrect data read";
+		}
+	}
+}
+
+TEST_F(RSeriesCPUDAQ, readDataBlockingTimeout){
+	const std::string bitfilePath = getBitfilePath();
+	const size_t blocksToRead = 1;
+	const std::uint32_t samplingRate = 500000;
+	const std::uint32_t channelSG = 0;
+	const std::uint32_t DMANum = 0;
+	const std::uint32_t analogNum = 0;
+	const std::uint32_t valDCSG = 2048;
+	const std::uint8_t signalType = 0; //DC
+
+	Irio irio(bitfilePath, serialNumber, "V1.2");
+	const auto commonTerm = irio.getTerminalsCommon();
+	irio.startFPGA();
+	commonTerm.setDebugMode(false);
+
+	auto sg = irio.getTerminalsSignalGeneration();
+	auto analog = irio.getTerminalsAnalog();
+	auto daq = irio.getTerminalsDAQ();
+
+	// Enable AO0 and set to 2048 and configure SGSignalType0
+	sg.setSGSignalType(channelSG, signalType);
+	analog.setAO(analogNum, valDCSG);
+	analog.setAOEnable(analogNum, true);
+
+	// DMA DAQ Parameters
+	const auto lengthBlock = daq.getLengthBlock(DMANum);
+
+	// Setup DMAQ
+	const std::uint16_t decimation = commonTerm.getFref()/samplingRate;
+	std::unique_ptr<std::uint64_t> data(new std::uint64_t[lengthBlock*blocksToRead]);
+	const auto timeout = static_cast<std::uint32_t>(std::ceil(lengthBlock*blocksToRead*1000/samplingRate));
+
+	sleep(5);
+	daq.cleanDMA(DMANum);
+
+	daq.startDMA(DMANum);
+	daq.setSamplingRateDecimation(DMANum, decimation);
+	daq.enableDMA(DMANum);
+
+	EXPECT_THROW(daq.readDataBlocking(DMANum, lengthBlock*blocksToRead, data.get(), timeout); // @suppress("Goto statement used")
+					, errors::DMAReadTimeout) << "Timeout exception was not raised";
+
+	daq.disableDMA(DMANum);
+	commonTerm.setDAQStop();
+	sleep(5);
+	daq.cleanDMA(DMANum);
+}
+
+/// Error RSeries DAQ Tests
 
 TEST_F(RSeriesCPUDAQError, InvalidAuxAIO) {
 	const std::string bitfilePath = getBitfilePath();
